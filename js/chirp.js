65d842ac173338593eb48ed8324455b2faa26705
nms377 <nms377@nyu.edu>
2017-02-04 12:26:47 -1000

    Fixed the function to return chirp chirp chirp. At first, I was just returning n which returned the number, and outside of the if statement, I was only returning the chirp funciton rather than  chirp + function.

--- js/chirp.js
+++ js/chirp.js
@@ -1,5 +1,15 @@
 function chirp(n){
   // FIX ME
+
+  //base case
+  if(n === 1){
+  	console.log(n);
+  	return 'chirp';	//return a string 'chirp'
+  }
+
+  console.log(n);
+	return 'chirp ' + chirp(--n);	//
+
 }
 
 $(document).ready(function(){
